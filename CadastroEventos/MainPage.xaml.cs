1b976547aa77bb9c4cbcbdba7505a2d7013f55d3
Maurotjr <maurotjr@hotmail.com>
2024-11-16 01:08:08 -0300

    Adicionar arquivos de projeto.

--- CadastroEventos/MainPage.xaml.cs
+++ CadastroEventos/MainPage.xaml.cs
@@ -8,7 +8,24 @@ namespace CadastroEventos
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = new Evento();                      
+            BindingContext = new Evento();
+            tpInicio.PropertyChanged += OnInicioTimeChanged;
+        }
+
+        private void OnInicioTimeChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(TimePicker.Time) && dpInicio != null && dpTermino != null)
+            {
+                // Verifica se as datas de início e término são iguais
+                if (dpInicio.Date == dpTermino.Date)
+                {
+                    // Ajusta o horário de término se estiver antes do início
+                    if (tpTermino.Time < tpInicio.Time)
+                    {
+                        tpTermino.Time = tpInicio.Time;
+                    }
+                }
+            }
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
